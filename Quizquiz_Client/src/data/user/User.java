64823d68f41045b500cc5d2b7f8c5becc73fb3e7
HeeSang <heescool@gmail.com>
2018-05-28 19:37:08 +0900

    Update User.java

--- Quizquiz_Client/src/data/user/User.java
+++ Quizquiz_Client/src/data/user/User.java
@@ -8,4 +8,32 @@ public class User extends Identification {
 	public int score;
 	
 	public List<User> friends;
+	
+	public User() {
+		
+	}
+	
+	public User(String id, String password) {
+		super(id, password);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		User otherUser = (User) obj;
+		
+		if (this.id.equals(otherUser.id) && this.password.equals(otherUser.password)) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public int hashCode() {
+		return this.id.hashCode();
+	}
+	
+	
 }
+
+
